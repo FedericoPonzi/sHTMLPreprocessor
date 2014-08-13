1d3aebac586a4ad2e11b6347302bcae35999eb6c
federicoponzi <kd.io>
2014-08-13 16:46:34 +0000

    Updated to run a simple include system.

--- run.py
+++ run.py
@@ -1,19 +1,35 @@
 import os.path
-def getFooter():
-	with open('html/footer.html') as footer:
-		return footer.readlines()
-def getHeaders():
-	with open('html/header.html') as header:
-		return header.readlines()
+import re
 
-filesToCompile = os.listdir("html")
-for f in os.listdir("html"):
-	if f == "footer.html" or f == "header.html":
-		continue
-	with open("html" + os.sep + f, "r") as file:
-		with open("compiled" + os.sep + f, "w") as compiled:
-			toWrite= getHeaders() + file.readlines() + getFooter()
-			print toWrite
-			compiled.writelines(toWrite)
+#Input: name of the file (complete with .html extension)
+#Output: the rows of the file.
+def getFile(toGet):
+	with open('html' + os.sep + toGet) as fileToGet:
+		return fileToGet.readlines()
+#Input: the file to compile
+#Return the compiled html file.
 
+def getToWrite(toCompile):
+    #This is the regex used to find the prep statments
+    regex = "<\?prep include \"([a-z]*\.[a-z]*)\"\?>"
+    #String to return:
+    toReturn = ""
+    #Scan all the lines and try to find the pattern
+    for f in toCompile:
+        tuples  = re.findall(regex, f)
+    
+        for el in tuples:
+            toReplace=''.join(getFile(el))
+            f = re.sub(regex, toReplace, f)
+    
+        toReturn = toReturn + f    
+    return toReturn
 
+#Main:
+if __name__ == "__main__":
+    for f in os.listdir("html"):
+    	with open("html" + os.sep + f, "rU") as file:
+    		toWrite = getToWrite(file.readlines())
+    		with open("compiled" + os.sep + f, "w") as compiled:
+    			compiled.writelines(toWrite)
+    print "Done! :)"
